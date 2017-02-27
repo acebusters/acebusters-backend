5a0ea69c04655af9b897acad529463995ab69da8
Johann Barbie <johannbarbie@me.com>
2017-02-27 15:22:12 +0100

    added blockchain tx

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+var AWS = require('aws-sdk');
 const StreamWorker = require('./lib/index');
+const Provider = require('./lib/provider');
+const Table = require('./lib/tableContract');
+
+var provider;
 
 exports.handler = function(event, context, callback) {
 
   if (event.Records && event.Records instanceof Array) {
+    
+    if (!provider) {
+      provider = new Provider(process.env.providerUrl,  process.env.recKey);
+    }
+
     var requests = [];
-    var worker = new StreamWorker();
+    var worker = new StreamWorker(new Table(provider));
     for (var i = 0; i < event.Records.length; i++) {
       requests.push(worker.process(event.Records[i]));
     }
--- index.test.js
+++ index.test.js
@@ -2,15 +2,33 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 require('chai').use(require('sinon-chai'));
 const StreamWorker = require('./lib/index');
+const TableContract = require('./lib/tableContract');
+
+var contract = {
+  leave: {
+    sendTransaction: function(){}, 
+  },
+}
+
+var provider = {
+  getTable: function(){},
+  getAddress: function(){},
+}
 
 describe('Stream worker', function() {
 
-  it('should notice lastHand change.', (done) => {
+  beforeEach(function () {
+    sinon.stub(provider, 'getTable').returns(contract);
+  });
 
+  it('should notice lastHand change.', (done) => {
 
     const event = {
       eventName: "MODIFY",
       dynamodb: {
+        Keys: {
+          tableAddr: { S: "0xa2decf075b96c8e5858279b31f644501a140e8a7" }
+        },
         NewImage: {
           lineup: { L: [
             { M: { address: { S: '0x82e8c6cf42c8d1ff9594b17a3f50e94a12cc860f' } } },
@@ -22,7 +40,7 @@ describe('Stream worker', function() {
                 N: '0'
               },
               leaveReceipt: {
-                S: '0x1234'
+                S: '0x99'
               }
             }},
           ]}
@@ -35,12 +53,22 @@ describe('Stream worker', function() {
         }
       }
     }
+    sinon.stub(contract.leave, 'sendTransaction').yields(null, '0x123456');
+    sinon.stub(provider, 'getAddress').returns('0x7777');
 
-    new StreamWorker().process(event).then(function(receipt) {
-      expect(receipt).to.eql('0x1234');
+    const worker = new StreamWorker(new TableContract(provider));    
+
+    worker.process(event).then(function(receipt) {
+      expect(receipt).to.eql('0x123456');
+      expect(contract.leave.sendTransaction).calledWith('0x99', {from: '0x7777', gas: sinon.match.any}, sinon.match.any);
       done();
     }).catch(done);
 
+  });
+
+  afterEach(function () {
+    if (contract.leave.sendTransaction.restore) contract.leave.sendTransaction.restore();
+    if (provider.getTable.restore) provider.getTable.restore();
   });
 
 });
