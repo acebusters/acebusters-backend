51e7e5d3efeeaa9ff25a318e398aba1f75ff4530
Johann Barbie <johannbarbie@me.com>
2017-02-27 23:37:37 +0100

    started on netting

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const StreamWorker = require('./lib/index');
 const Provider = require('./lib/provider');
 const Table = require('./lib/tableContract');
 
-var provider;
+const TABLE_ABI = [{"constant":false,"inputs":[{"name":"_leaveReceipt","type":"bytes"}],"name":"leave","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"seats","outputs":[{"name":"senderAddr","type":"address"},{"name":"amount","type":"uint96"},{"name":"signerAddr","type":"address"},{"name":"lastHand","type":"uint96"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"lastNettingRequestTime","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"lastHandNetted","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_sender","type":"address"}],"name":"payoutFrom","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_newBalances","type":"bytes"},{"name":"_sigs","type":"bytes"}],"name":"settle","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"payout","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_now","type":"uint256"}],"name":"netHelp","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"oracle","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"hands","outputs":[{"name":"claimCount","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_buyIn","type":"uint96"},{"name":"_signerAddr","type":"address"},{"name":"_pos","type":"uint256"}],"name":"join","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getLineup","outputs":[{"name":"","type":"uint256"},{"name":"addr","type":"address[]"},{"name":"amount","type":"uint256[]"},{"name":"lastHand","type":"uint96[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_bets","type":"bytes"},{"name":"_sigs","type":"bytes"}],"name":"submitBets","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"lastNettingRequestHandId","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"net","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"seatMap","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_handId","type":"uint96"},{"name":"_addr","type":"address"}],"name":"getIn","outputs":[{"name":"","type":"uint96"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"smallBlind","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_handId","type":"uint96"},{"name":"_addr","type":"address"}],"name":"getOut","outputs":[{"name":"","type":"uint96"},{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_dists","type":"bytes"},{"name":"_sigs","type":"bytes"}],"name":"submitDists","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"token","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"_token","type":"address"},{"name":"_oracle","type":"address"},{"name":"_smallBlind","type":"uint256"},{"name":"_seats","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"addr","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"Join","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"hand","type":"uint256"}],"name":"NettingRequest","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"hand","type":"uint256"}],"name":"Netted","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"errorCode","type":"uint256"}],"name":"Error","type":"event"}];
+
+var web3Provider;
 
 exports.handler = function(event, context, callback) {
 
   if (event.Records && event.Records instanceof Array) {
-    
-    if (!provider) {
-      provider = new Provider(process.env.providerUrl,  process.env.recKey);
+
+    if (!web3Provider) {
+      var web3 = new Web3();
+      web3Provider = new web3.providers.HttpProvider(process.env.providerUrl);
     }
+    var provider = new Provider(web3Provider, process.env.accountAddr, TABLE_ABI);
 
     var requests = [];
     var worker = new StreamWorker(new Table(provider));
--- index.test.js
+++ index.test.js
@@ -4,6 +4,23 @@ require('chai').use(require('sinon-chai'));
 const StreamWorker = require('./lib/index');
 const TableContract = require('./lib/tableContract');
 
+const EWT = require('ethereum-web-token');
+
+// BET can replace lower bet
+// BET can replace SIT_OUT during dealing state
+const ABI_BET = [{name: 'bet', type: 'function', inputs: [{type: 'uint'}, {type: 'uint'}]}];
+
+// FOLD can replace all but SIT_OUT, and SHOW, given same amount
+const ABI_FOLD = [{name: 'fold', type: 'function', inputs: [{type: 'uint'}, {type: 'uint'}]}];
+
+const P1_ADDR = '0xf3beac30c498d9e26865f34fcaa57dbb935b0d74';
+const P1_KEY = '0x278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f';
+
+//secretSeed: 'brother mad churn often amount wing pretty critic rhythm man insane ridge' }
+const P2_ADDR = '0xe10f3d125e5f4c753a6456fc37123cf17c6900f2';
+const P2_KEY = '0x7bc8feb5e1ce2927480de19d8bc1dc6874678c016ae53a2eec6a6e9df717bfac';
+
+
 var contract = {
   leave: {
     sendTransaction: function(){}, 
@@ -21,7 +38,7 @@ describe('Stream worker', function() {
     sinon.stub(provider, 'getTable').returns(contract);
   });
 
-  it('should notice lastHand change.', (done) => {
+  it('should send tx on new leave receipt.', (done) => {
 
     const event = {
       eventName: "MODIFY",
@@ -52,18 +69,55 @@ describe('Stream worker', function() {
           ]}
         }
       }
-    }
+    };
     sinon.stub(contract.leave, 'sendTransaction').yields(null, '0x123456');
     sinon.stub(provider, 'getAddress').returns('0x7777');
 
-    const worker = new StreamWorker(new TableContract(provider));    
+    const worker = new StreamWorker(new TableContract(provider));
 
-    worker.process(event).then(function(receipt) {
-      expect(receipt).to.eql('0x123456');
+    worker.process(event).then(function(tx) {
+      expect(tx).to.eql('0x123456');
       expect(contract.leave.sendTransaction).calledWith('0x99', {from: '0x7777', gas: sinon.match.any}, sinon.match.any);
       done();
     }).catch(done);
 
+  });
+
+  it('should notify when hand with leaving player turns complete.', (done) => {
+    var bet1 = new EWT(ABI_BET).bet(1, 50).sign(P1_KEY);
+    var bet2 = new EWT(ABI_BET).bet(1, 100).sign(P2_KEY);
+    var fold = new EWT(ABI_FOLD).fold(1, 50).sign(P1_KEY);
+
+    var lineup = [
+      {address: P1_ADDR, last: bet1},
+      {address: P2_ADDR, last: bet2}
+    ];
+
+    const event = {
+      eventName: "MODIFY",
+      dynamodb: {
+        OldImage: {
+          dealer: { N: '0' },
+          lineup: { L: [
+            { M: { address: { S: P1_ADDR }, last: { S: bet1 } } },
+            { M: { address: { S: P2_ADDR }, last: { S: bet2 }, lastHand: { N: '1' }, leaveReceipt: { S: '0x99' } } }
+          ]}
+        },
+        NewImage: {
+          dealer: { N: '0' },
+          lineup: { L: [
+            { M: { address: { S: P1_ADDR }, last: { S: bet1 } } },
+            { M: { address: { S: P2_ADDR }, last: { S: fold }, lastHand: { N: '1' }, leaveReceipt: { S: '0x99' } } }
+          ]}
+        }
+      }
+    };
+
+    const worker = new StreamWorker();
+    worker.process(event).then(function(rsp) {
+      expect(rsp).to.eql('now');
+      done();
+    }).catch(done);
   });
 
   afterEach(function () {
@@ -72,3 +126,131 @@ describe('Stream worker', function() {
   });
 
 });
+
+
+// {
+//     "Records": [
+//         {
+//             "eventID": "db2920985bc6ba7f5deef8d028876d5e",
+//             "eventName": "MODIFY",
+//             "eventVersion": "1.1",
+//             "eventSource": "aws:dynamodb",
+//             "awsRegion": "eu-west-1",
+//             "dynamodb": {
+//                 "ApproximateCreationDateTime": 1488198300,
+//                 "Keys": {
+//                     "handId": {
+//                         "N": "4"
+//                     },
+//                     "tableAddr": {
+//                         "S": "0xa2decf075b96c8e5858279b31f644501a140e8a7"
+//                     }
+//                 },
+//                 "NewImage": {
+//                     "deck": {},
+//                     "dealer": "0",
+//                     "handId": "4",
+//                     "lineup": {
+//                         "L": [
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0x82e8c6cf42c8d1ff9594b17a3f50e94a12cc860f"
+//                                     },
+//                                     "last": {
+//                                         "S": "eyJ0eXBlIjoiRVdUIiwiYWxnIjoiRVMyNTZrIn0.eyJiZXQiOlt7InVpbnQiOjR9LHsidWludCI6NTAwMDB9XSwidiI6MX0.aTQuKTw5m94EoDd6Ucn2qA7eRKdL1AtrjKErgZHOQXJtX6J7tRjWzhB14-sLcS0TpaK9ENT0LmN1c4Z9IlLWyg"
+//                                     }
+//                                 }
+//                             },
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0xc3ccb3902a164b83663947aff0284c6624f3fbf2"
+//                                     },
+//                                     "last": {
+//                                         "S": "eyJ0eXBlIjoiRVdUIiwiYWxnIjoiRVMyNTZrIn0.eyJiZXQiOlt7InVpbnQiOjR9LHsidWludCI6NTAwMDB9XSwidiI6MX0.aTQuKTw5m94EoDd6Ucn2qA7eRKdL1AtrjKErgZHOQXJtX6J7tRjWzhB14-sLcS0TpaK9ENT0LmN1c4Z9IlLWyg"
+//                                     }
+//                                 }
+//                             },
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0xe10f3d125e5f4c753a6456fc37123cf17c6900f2"
+//                                     }
+//                                 }
+//                             },
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0xf3beac30c498d9e26865f34fcaa57dbb935b0d74"
+//                                     }
+//                                 }
+//                             }
+//                         ]
+//                     },
+//                     "handState": {
+//                         "S": "dealing"
+//                     },
+//                     "tableAddr": {
+//                         "S": "0xa2decf075b96c8e5858279b31f644501a140e8a7"
+//                     }
+//                 },
+//                 "OldImage": {
+//                     "deck": {
+//                     },
+//                     "dealer": {
+//                         "N": "0"
+//                     },
+//                     "handId": {
+//                         "N": "4"
+//                     },
+//                     "lineup": {
+//                         "L": [
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0x82e8c6cf42c8d1ff9594b17a3f50e94a12cc860f"
+//                                     }
+//                                 }
+//                             },
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0xc3ccb3902a164b83663947aff0284c6624f3fbf2"
+//                                     },
+//                                     "last": {
+//                                         "S": "eyJ0eXBlIjoiRVdUIiwiYWxnIjoiRVMyNTZrIn0.eyJiZXQiOlt7InVpbnQiOjR9LHsidWludCI6NTAwMDB9XSwidiI6MX0.aTQuKTw5m94EoDd6Ucn2qA7eRKdL1AtrjKErgZHOQXJtX6J7tRjWzhB14-sLcS0TpaK9ENT0LmN1c4Z9IlLWyg"
+//                                     }
+//                                 }
+//                             },
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0xe10f3d125e5f4c753a6456fc37123cf17c6900f2"
+//                                     }
+//                                 }
+//                             },
+//                             {
+//                                 "M": {
+//                                     "address": {
+//                                         "S": "0xf3beac30c498d9e26865f34fcaa57dbb935b0d74"
+//                                     }
+//                                 }
+//                             }
+//                         ]
+//                     },
+//                     "handState": {
+//                         "S": "dealing"
+//                     },
+//                     "tableAddr": {
+//                         "S": "0xa2decf075b96c8e5858279b31f644501a140e8a7"
+//                     }
+//                 },
+//                 "SequenceNumber": "742292900000000001062735493",
+//                 "SizeBytes": 1558,
+//                 "StreamViewType": "NEW_AND_OLD_IMAGES"
+//             },
+//             "eventSourceARN": "arn:aws:dynamodb:eu-west-1:105751009136:table/poker/stream/2017-02-27T12:13:35.529"
+//         }
+//     ]
+// }
