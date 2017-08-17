1b467448ea3d7573f17fee9488d4b26cbb20893e
Alex Lunyov <isuntc@gmail.com>
2017-08-17 18:28:52 +0800

    Refactor contract modules

--- src/tableContract.js
+++ src/tableContract.js
@@ -1,60 +1,30 @@
+import Contract from './contract';
+
 const TABLE_ABI = [{ constant: false, inputs: [{ name: '_leaveReceipt', type: 'bytes' }], name: 'leave', outputs: [], payable: false, type: 'function' }, { constant: true, inputs: [{ name: '', type: 'uint256' }], name: 'seats', outputs: [{ name: 'senderAddr', type: 'address' }, { name: 'amount', type: 'uint96' }, { name: 'signerAddr', type: 'address' }, { name: 'lastHand', type: 'uint96' }], payable: false, type: 'function' }, { constant: true, inputs: [], name: 'lastNettingRequestTime', outputs: [{ name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: true, inputs: [], name: 'lastHandNetted', outputs: [{ name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: false, inputs: [{ name: '_sender', type: 'address' }], name: 'payoutFrom', outputs: [], payable: false, type: 'function' }, { constant: false, inputs: [{ name: '_newBalances', type: 'bytes' }, { name: '_sigs', type: 'bytes' }], name: 'settle', outputs: [], payable: false, type: 'function' }, { constant: false, inputs: [], name: 'payout', outputs: [], payable: false, type: 'function' }, { constant: false, inputs: [{ name: '_now', type: 'uint256' }], name: 'netHelp', outputs: [], payable: false, type: 'function' }, { constant: true, inputs: [], name: 'oracle', outputs: [{ name: '', type: 'address' }], payable: false, type: 'function' }, { constant: true, inputs: [{ name: '', type: 'uint256' }], name: 'hands', outputs: [{ name: 'claimCount', type: 'uint256' }], payable: false, type: 'function' }, { constant: false, inputs: [{ name: '_buyIn', type: 'uint96' }, { name: '_signerAddr', type: 'address' }, { name: '_pos', type: 'uint256' }], name: 'join', outputs: [], payable: false, type: 'function' }, { constant: true, inputs: [], name: 'getLineup', outputs: [{ name: '', type: 'uint256' }, { name: 'addr', type: 'address[]' }, { name: 'amount', type: 'uint256[]' }, { name: 'lastHand', type: 'uint96[]' }], payable: false, type: 'function' }, { constant: false, inputs: [{ name: '_bets', type: 'bytes' }, { name: '_sigs', type: 'bytes' }], name: 'submitBets', outputs: [{ name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: true, inputs: [], name: 'lastNettingRequestHandId', outputs: [{ name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: false, inputs: [], name: 'net', outputs: [], payable: false, type: 'function' }, { constant: true, inputs: [{ name: '', type: 'address' }], name: 'seatMap', outputs: [{ name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: true, inputs: [{ name: '_handId', type: 'uint96' }, { name: '_addr', type: 'address' }], name: 'getIn', outputs: [{ name: '', type: 'uint96' }], payable: false, type: 'function' }, { constant: true, inputs: [], name: 'smallBlind', outputs: [{ name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: true, inputs: [{ name: '_handId', type: 'uint96' }, { name: '_addr', type: 'address' }], name: 'getOut', outputs: [{ name: '', type: 'uint96' }, { name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: false, inputs: [{ name: '_dists', type: 'bytes' }, { name: '_sigs', type: 'bytes' }], name: 'submitDists', outputs: [{ name: '', type: 'uint256' }], payable: false, type: 'function' }, { constant: true, inputs: [], name: 'token', outputs: [{ name: '', type: 'address' }], payable: false, type: 'function' }, { inputs: [{ name: '_token', type: 'address' }, { name: '_oracle', type: 'address' }, { name: '_smallBlind', type: 'uint256' }, { name: '_seats', type: 'uint256' }], payable: false, type: 'constructor' }, { anonymous: false, inputs: [{ indexed: false, name: 'addr', type: 'address' }, { indexed: false, name: 'amount', type: 'uint256' }], name: 'Join', type: 'event' }, { anonymous: false, inputs: [{ indexed: false, name: 'hand', type: 'uint256' }], name: 'NettingRequest', type: 'event' }, { anonymous: false, inputs: [{ indexed: false, name: 'hand', type: 'uint256' }], name: 'Netted', type: 'event' }, { anonymous: false, inputs: [{ indexed: false, name: 'errorCode', type: 'uint256' }], name: 'Error', type: 'event' }];
 
-function TableContract(web3) {
-  this.web3 = web3;
+export default class TableContract extends Contract {
+  getLastHandNetted(tableAddr) {
+    const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
+    return this.call(contract.lastHandNetted.call).then(n => n.toNumber());
+  }
+
+  // get last netting request hand id
+  getLNRHandId(tableAddr) {
+    const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
+    return this.call(contract.lastNettingRequestHandId.call).then(n => n.toNumber());
+  }
+
+  // get last netting request time
+  getLNRTime(tableAddr) {
+    const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
+    return this.call(contract.lastNettingRequestTime.call).then(n => n.toNumber());
+  }
+
+  getLineup(tableAddr) {
+    const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
+    return this.call(contract.getLineup.call).then(lineup => [
+      ...lineup.slice(0, 3),
+      lineup[3].map(n => n.toNumber()),
+    ]);
+  }
 }
-
-TableContract.prototype.getLastHandNetted = function getLastHandNetted(tableAddr) {
-  const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
-  return new Promise((fulfill, reject) => {
-    contract.lastHandNetted.call((err, val) => {
-      if (err) {
-        reject(err);
-      }
-      fulfill(val.toNumber());
-    });
-  });
-};
-
-// get last netting request hand id
-TableContract.prototype.getLNRHandId = function getLNRHandId(tableAddr) {
-  const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
-  return new Promise((fulfill, reject) => {
-    contract.lastNettingRequestHandId.call((err, val) => {
-      if (err) {
-        reject(err);
-      }
-      fulfill(val.toNumber());
-    });
-  });
-};
-
-// get last netting request time
-TableContract.prototype.getLNRTime = function getLNRTime(tableAddr) {
-  const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
-  return new Promise((fulfill, reject) => {
-    contract.lastNettingRequestTime.call((err, val) => {
-      if (err) {
-        reject(err);
-      }
-      fulfill(val.toNumber());
-    });
-  });
-};
-
-TableContract.prototype.getLineup = function getLineup(tableAddr) {
-  const contract = this.web3.eth.contract(TABLE_ABI).at(tableAddr);
-  return new Promise((fulfill, reject) => {
-    contract.getLineup.call((err, val) => {
-      if (err) {
-        reject(err);
-      }
-      fulfill([
-        ...val.slice(0, 3),
-        val[3].map(n => n.toNumber()),
-      ]);
-    });
-  });
-};
-
-module.exports = TableContract;
