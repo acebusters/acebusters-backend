2a4f934ce3c5b4cf902938fb35476bbc83708883
Kosta Korenkov <kosta@whoot.me>
2017-10-16 13:31:30 +0300

    should not emit TableNettingComplete if hand.is_netted
    we want settle tx to be sent once per netting round. So we check is_netted flag before settlement. If it is true, we assume settlement tx was already sent. See corresponding event-worker change

--- index.test.js
+++ index.test.js
@@ -21,6 +21,9 @@ const P1_PRIV = '0x278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d253
 const P2_ADDR = '0xe10f3d125e5f4c753a6456fc37123cf17c6900f2';
 const P2_PRIV = '0x7bc8feb5e1ce2927480de19d8bc1dc6874678c016ae53a2eec6a6e9df717bfac';
 
+const P3_ADDR = '0xb7164eD7ce81F1940923F3fc9c1e50F703840863';
+const P3_PRIV = '0xc6d34959ee31f2e2577aa678ef8b8c3cda3176727c2e3588c95e99e9d95714d3';
+
 const ORACLE_ADDR = '0x82e8c6cf42c8d1ff9594b17a3f50e94a12cc860f';
 
 const EMPTY_ADDR = '0x0000000000000000000000000000000000000000';
@@ -470,6 +473,64 @@ describe('Stream scanner', () => {
         }),
         TopicArn: topicArn,
       });
+      done();
+    }).catch(done);
+  });
+
+  it('should not submit settlement if hand is marked as netted.', (done) => {
+    const bet1 = new Receipt(EMPTY_ADDR).bet(2, babz(1000)).sign(P1_PRIV);
+    const bet2 = new Receipt(EMPTY_ADDR).bet(2, babz(1000)).sign(P2_PRIV);
+    const event = {
+      eventName: 'MODIFY',
+      dynamodb: {
+        Keys: {
+          tableAddr: {
+            S: '0x77aabb11ee00',
+          },
+        },
+        OldImage: {
+          dealer: { N: '0' },
+          handId: { N: '2' },
+          state: { S: 'preflop' },
+          lineup: { L: [
+            { M: { address: { S: EMPTY_ADDR } } },
+            { M: { address: { S: P1_ADDR }, last: { S: bet1 }, sitout: { S: 'allin' } } },
+            { M: { address: { S: P2_ADDR }, last: { S: bet2 } } },
+            { M: { address: { S: EMPTY_ADDR } } },
+          ] },
+          netting: { M: {
+            newBalances: { S: '0x112233' },
+            [ORACLE_ADDR]: { S: '0x223344' },
+            [P1_ADDR]: { S: '0x334455' },
+          } },
+        },
+        NewImage: {
+          dealer: { N: '0' },
+          handId: { N: '2' },
+          state: { S: 'showdown' },
+          deck: { L: [{ N: 0 }, { N: 1 }, { N: 2 }, { N: 3 }] },
+          lineup: { L: [
+            { M: { address: { S: EMPTY_ADDR } } },
+            { M: { address: { S: P1_ADDR }, last: { S: bet1 }, sitout: { S: 'allin' } } },
+            { M: { address: { S: P2_ADDR }, last: { S: bet2 } } },
+            { M: { address: { S: EMPTY_ADDR } } },
+          ] },
+          is_netted: { B: true },
+          netting: { M: {
+            newBalances: { S: '0x112233' },
+            [ORACLE_ADDR]: { S: '0x223344' },
+            [P1_ADDR]: { S: '0x334455' },
+            [P2_ADDR]: { S: '0x445566' },
+          } },
+        },
+      },
+    };
+    sinon.stub(sns, 'publish').yields(null, {});
+
+    const worker = new StreamWorker(sns, topicArn, pusher, rc);
+
+    worker.process(event).then(() => {
+      expect(sns.publish).callCount(0);
       done();
     }).catch(done);
   });
