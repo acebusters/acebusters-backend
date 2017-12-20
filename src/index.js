5cf3afa7d570e26cab60359ce84dc2133a397be8
Alex Lunyov <isuntc@gmail.com>
2017-12-20 15:18:58 +0800

    Extract utils

--- src/index.js
+++ src/index.js
@@ -1,55 +1,19 @@
 import ethUtil from 'ethereumjs-util';
 import 'buffer-v6-polyfill';
-import crypto from 'crypto';
 import BigNumber from 'bignumber.js';
 import { PokerHelper, Receipt, ReceiptCache } from 'poker-helper';
-
-const delay = (ms, arg) => new Promise(resolve => setTimeout(resolve, ms, arg));
-
-const EMPTY_ADDR = '0x0000000000000000000000000000000000000000';
-const isEmpty = seat => seat.address === EMPTY_ADDR;
-const hasReceipt = seat => !!seat.last;
-const not = fn => (...args) => !fn(...args);
-
-function now(secs = 0) {
-  return Math.floor(Date.now() / 1000) + secs;
-}
-
-const shuffle = function shuffle() {
-  const array = [];
-  for (let i = 0; i < 52; i += 1) {
-    array.push(i);
-  }
-  for (let i = array.length - 1; i > 0; i -= 1) {
-    const j = crypto.randomBytes(1)[0] % i;
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-};
-
-function range(s, e) {
-  return Array.from(new Array((e - s) + 1), (_, i) => i + s);
-}
-
-function identity(a) {
-  return a;
-}
-
-function parseMessage(msg) {
-  if (!msg.Subject || msg.Subject.split('::').length < 2) {
-    throw new Error(`unknown message type: ${msg.Subject}`);
-  }
-  try {
-    return {
-      msgType: msg.Subject.split('::')[0],
-      msgBody: (msg.Message && msg.Message.length > 0) ? JSON.parse(msg.Message) : '',
-    };
-  } catch (e) {
-    throw new Error(`json parse error: ${JSON.stringify(e)}`);
-  }
-}
+import {
+  parseMessage,
+  now,
+  range,
+  identity,
+  not,
+  isEmpty,
+  hasReceipt,
+  delay,
+  shuffle,
+  EMPTY_ADDR,
+} from './utils';
 
 class EventWorker {
   constructor(
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,52 @@
+import crypto from 'crypto';
+
+export const delay = (ms, arg) => new Promise(resolve => setTimeout(resolve, ms, arg));
+
+export const EMPTY_ADDR = '0x0000000000000000000000000000000000000000';
+export const isEmpty = seat => seat.address === EMPTY_ADDR;
+export const hasReceipt = seat => !!seat.last;
+export const not = fn => (...args) => !fn(...args);
+
+export function now(secs = 0) {
+  return Math.floor(Date.now() / 1000) + secs;
+}
+
+export const shuffle = function shuffle() {
+  const array = [];
+  for (let i = 0; i < 52; i += 1) {
+    array.push(i);
+  }
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    const j = crypto.randomBytes(1)[0] % i;
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+export function range(s, e) {
+  return Array.from(new Array((e - s) + 1), (_, i) => i + s);
+}
+
+export function identity(a) {
+  return a;
+}
+
+export function parseMessage(msg) {
+  if (!msg.Subject || msg.Subject.split('::').length < 2) {
+    throw new Error(`unknown message type: ${msg.Subject}`);
+  }
+  try {
+    return {
+      msgType: msg.Subject.split('::')[0],
+      msgBody: (msg.Message && msg.Message.length > 0) ? JSON.parse(msg.Message) : '',
+    };
+  } catch (e) {
+    throw new Error(`json parse error: ${JSON.stringify(e)}`);
+  }
+}
+
 export function dbMethod(provider, name, params) {
   return new Promise((resolve, reject) => {
     provider[name](params, (err, result) => {
