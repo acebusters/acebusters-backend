b3fe303fc17aa232ca7b3b9ef2704afbc8f3facd
Alex Lunyov <isuntc@gmail.com>
2018-02-15 14:00:47 +0800

    Simple solution for preventing duplicate requests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import BigNumber from 'bignumber.js';
 const babz = ntz => new BigNumber(10).pow(12).mul(ntz);
 const wei = eth => new BigNumber(10).pow(18).mul(eth);
 
+const fundingTimestamps = {};
+
 export default class Faucet {
   constructor(nutz, logger, ntzThreshold = 1000, ethThreshold = 0.1) {
     this.nutz = nutz;
@@ -16,18 +18,27 @@ export default class Faucet {
   async requestFunds(signerAddr) {
     const tasks = [];
 
-    const babzBalance = await this.nutz.balanceOf(signerAddr);
-    if (this.babzThreshold.gt(babzBalance)) {
-      tasks.push(this.nutz.transfer(signerAddr, this.babzThreshold.sub(babzBalance)));
+    const tenMinutesAgo = Date.now() - (10 * 60 * 1000);
+    if (
+      !fundingTimestamps[signerAddr] ||
+      fundingTimestamps[signerAddr] < tenMinutesAgo
+    ) {
+      const babzBalance = await this.nutz.balanceOf(signerAddr);
+      if (this.babzThreshold.gt(babzBalance)) {
+        tasks.push(this.nutz.transfer(signerAddr, this.babzThreshold.sub(babzBalance)));
+      }
+
+      const weiBalance = await this.nutz.weiBalance(signerAddr);
+      if (this.weiThreshold.gt(weiBalance)) {
+        tasks.push(this.nutz.sendEth(signerAddr, this.weiThreshold.sub(weiBalance)));
+      }
     }
 
-    const weiBalance = await this.nutz.weiBalance(signerAddr);
-    if (this.weiThreshold.gt(weiBalance)) {
-      tasks.push(this.nutz.sendEth(signerAddr, this.weiThreshold.sub(weiBalance)));
+    if (tasks.length > 0) {
+      fundingTimestamps[signerAddr] = Date.now();
     }
-
     await Promise.all(tasks);
 
-    return true;
+    return tasks.length > 0;
   }
 }
